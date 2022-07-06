60af33e49cf0a35a7b0bb5b35e1b941962b14d8f
magi-1 <mechior.magi@gmail.com>
2022-07-06 00:27:10 -0500

    cargo fmt

--- src/ball.rs
+++ src/ball.rs
@@ -25,7 +25,7 @@ pub struct Ball {
     pub phi: f64,
     pub mag_v: f64,
     pub r_coeffs: Array2<f64>,
-    pub v_coeffs: Array2<f64>
+    pub v_coeffs: Array2<f64>,
 }
 
 impl Ball {
@@ -38,8 +38,8 @@ impl Ball {
             v: array![0.0, 0.0],
             phi: 0.0,
             mag_v: 0.0,
-            r_coeffs: Array2::<f64>::zeros((3,2)),
-            v_coeffs: Array2::<f64>::zeros((2,2)),
+            r_coeffs: Array2::<f64>::zeros((3, 2)),
+            v_coeffs: Array2::<f64>::zeros((2, 2)),
         }
     }
 
@@ -49,16 +49,18 @@ impl Ball {
         self.mag_v = (&self.v.dot(&self.v)).sqrt();
         let cos_phi: f64 = self.phi.cos();
         let sin_phi: f64 = self.phi.sin();
-        let grav_fric: f64 = -G*MU;
-        let mag_v_cos_phi: f64 = self.mag_v*cos_phi;
-        let mag_v_sin_phi: f64 = self.mag_v*sin_phi;
+        let grav_fric: f64 = -G * MU;
+        let mag_v_cos_phi: f64 = self.mag_v * cos_phi;
+        let mag_v_sin_phi: f64 = self.mag_v * sin_phi;
         self.r_coeffs = array![
-            [self.r[0], mag_v_cos_phi, grav_fric*cos_phi], 
-            [self.r[1], mag_v_sin_phi, grav_fric*sin_phi]];
+            [self.r[0], mag_v_cos_phi, grav_fric * cos_phi],
+            [self.r[1], mag_v_sin_phi, grav_fric * sin_phi]
+        ];
         self.v_coeffs = array![
-            [mag_v_cos_phi, grav_fric*cos_phi],
-            [mag_v_sin_phi, grav_fric*sin_phi]];
-        
+            [mag_v_cos_phi, grav_fric * cos_phi],
+            [mag_v_sin_phi, grav_fric * sin_phi]
+        ];
+
         if self.mag_v < DELTA {
             self.bstate = BallState::STATIONARY;
         } else {
@@ -77,9 +79,10 @@ impl Ball {
     pub fn reset(&mut self) {
         self.r = array![0.0, 0.0];
         self.v = array![0.0, 0.0];
+        self.phi = 0.0;
         self.mag_v = 0.0;
-        self.r_coeffs = Array2::<f64>::zeros((3,2));
-        self.v_coeffs = Array2::<f64>::zeros((2,2));
+        self.r_coeffs = Array2::<f64>::zeros((3, 2));
+        self.v_coeffs = Array2::<f64>::zeros((2, 2));
         self.bstate = BallState::STATIONARY;
     }
 

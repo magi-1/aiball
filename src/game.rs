836bdbea944f2b6557e6fb3da816474dfa5752b3
magi-1 <mechior.magi@gmail.com>
2022-07-05 21:04:20 -0500

    cargo fmt

--- src/game.rs
+++ src/game.rs
@@ -16,24 +16,23 @@ impl Game {
     fn evaluate_game_state(&mut self) {}
 
     pub fn get_next_event(&self) -> Option<EventEnum> {
-        let objects: &SimObjects = &self.sim_objects;
-        let mut next_event: EventEnum = EventEnum::NullEvent(NullEvent::new());
+        let objects = &self.sim_objects;
+        let mut next_event = EventEnum::NullEvent(NullEvent::new());
         for ball_id in 0..objects.pool_balls.num_balls() {
             for pocket_id in 0..objects.pool_table.num_pockets() {
-                let mut event: EventEnum = EventEnum::HitPocket(HitPocket::new(ball_id, pocket_id));
+                let mut event = EventEnum::HitPocket(HitPocket::new(ball_id, pocket_id));
                 event.calculate_time_until(objects);
                 mut_compare_events(&mut next_event, event);
             }
 
             for cushion_id in 0..objects.pool_table.num_cushions() {
-                let mut event: EventEnum =
-                    EventEnum::HitCushion(HitCushion::new(ball_id, cushion_id));
+                let mut event = EventEnum::HitCushion(HitCushion::new(ball_id, cushion_id));
                 event.calculate_time_until(objects);
                 mut_compare_events(&mut next_event, event);
             }
 
             for other_ball_id in 0..objects.pool_balls.num_balls() {
-                let mut event: EventEnum = EventEnum::HitBall(HitBall::new(ball_id, other_ball_id));
+                let mut event = EventEnum::HitBall(HitBall::new(ball_id, other_ball_id));
                 event.calculate_time_until(objects);
                 mut_compare_events(&mut next_event, event);
             }
